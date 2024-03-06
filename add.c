d84d5ca157e8ed51081dcbac6ba7dc667bb83d65
jiyachouksey <162309021+jiyachouksey@users.noreply.github.com>
2024-03-06 01:53:52 -0800

    Add files via upload

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter number a =");
+    scanf("%d", &a);
+    printf("enter name b =");
+    scanf("%d", &b);
+
+    printf("add : %d", a + b);
+    return 0;
+}
